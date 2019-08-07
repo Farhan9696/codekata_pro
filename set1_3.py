cc16a2031bebe3c71f30c424f8b2abf9a7ff29f5
Farhan9696 <37727645+Farhan9696@users.noreply.github.com>
2019-08-07 20:54:50 +0530

    Update set1_3.py

--- set1_3.py
+++ set1_3.py
@@ -1,8 +1,20 @@
- a,b=input().split()
-char=abs(len(b)-len(a))
-for c in range(len(a)):
-  if(len(b)==1 and b[c] in a):
-    break
-  if(a[c]!=b[c]):
-    char=char+1
-print(char)
+f,s=list(map(str,input().split()))
+t=0
+if f==s: print(t)
+elif len(f)>len(s):
+    f=list(f)
+    s=list(s)
+    for i in range(0,len(s)):
+        for j in range(0,len(f)):
+            if f[j]==s[i]:
+                f.pop(j)
+                break
+    t=len(f)
+    print(t)
+else:
+    for i in range(0, min(len(f), len(s))):
+        if f[i] != s[i]:
+            t = t + 1
+        if i == min(len(f), len(s)) - 1:
+            t = t + abs((i + 1) - max(len(f), len(s)))
+    print(t)
